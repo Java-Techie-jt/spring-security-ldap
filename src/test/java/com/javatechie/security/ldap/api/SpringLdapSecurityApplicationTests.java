a041cc48982815d2b6e053bac23b5d1200ce0078
Basant kumar Hota <basahota@users.noreply.github.com>
2018-07-21 01:41:46 +0530

    #code

---
+++ src/test/java/com/javatechie/security/ldap/api/SpringLdapSecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javatechie.security.ldap.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringLdapSecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
